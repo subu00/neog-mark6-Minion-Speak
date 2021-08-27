c433b89e111ab22101145faa3380951f4323d787
subu00 <73189091+subu00@users.noreply.github.com>
2021-08-27 12:16:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const button = document.querySelector("#btn");
+const textAreaInput = document.querySelector("#textarea-input");
+const textAreaOutput = document.querySelector("#textarea-output")
+
+
+let serverURL = "https://api.funtranslations.com/translate/minion.json"; 
+
+function translateUrl(input) {
+    return serverURL + '?text=' +input;
+}
+
+function handleError(error) {
+    console.log("Error has occured", error)
+    alert("Please try agian somthing went wrong.")
+}
+
+
+function handleTranslateClick() {
+    console.log("Button was clicked")
+    let input  = textAreaInput.value;
+
+    fetch(translateUrl(input))
+        .then(response => response.json())
+        .then(json => {
+            let translatedOutput = json.contents.translated;
+            textAreaOutput.innerText = translatedOutput;
+        }).catch(handleError)
+
+}
+
+
+
+
+
+button.addEventListener('click', handleTranslateClick)
